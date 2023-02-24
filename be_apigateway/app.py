72a9f98c64f9ea4029d1de3388b6c4a7260bbdf3
Alejandro Figueroa <alejofig@hotmail.com>
2023-02-24 11:39:09 -0500

    added dockerfile

--- be_apigateway/app.py
+++ be_apigateway/app.py
@@ -2,6 +2,7 @@ from be_apigateway import create_app
 from .modelos import db
 from flask_restful import Api
 from .vistas import VistaClients, VistaPedidos, VistaResponse
+import serverless_wsgi
 
 
 app = create_app('default')
@@ -15,3 +16,6 @@ api = Api(app)
 api.add_resource(VistaClients, '/v1/clients/<int:vendedor_id>')
 api.add_resource(VistaPedidos, '/v1/pedidos/<int:vendedor_id>')
 api.add_resource(VistaResponse, '/v1/response')
+
+def handler(event, context):
+    return serverless_wsgi.handle_request(app, event, context)
