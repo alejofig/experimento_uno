133022858f9f0b41146d41a0d1649f2a790f1b62
Alejandro Figueroa <alejofig@hotmail.com>
2023-02-24 12:54:32 -0500

    changed request to apig

--- be_clients/app.py
+++ be_clients/app.py
@@ -21,7 +21,7 @@ def handler(event, context):
 
     create_event_and_return_message_id(
         message, TipoEvento.RECIBIDO, event['Records'][0]['Sns']['MessageId'])
-    Mock()
+    #Mock()
 
     clientes_vendedor = DarCliente().dar_clientes_vendedor(message["vendedor_id"])
     sns_queue = SNS("us-east-1")
@@ -29,8 +29,8 @@ def handler(event, context):
                        "data": clientes_vendedor}
     endpoint = "https://n3dox8jtg5.execute-api.us-east-1.amazonaws.com/dev/v1/response"
     data = json.dumps(message_to_send)
-    print(data)
-    response = requests.post(endpoint, json=data)
+    headers = {"Content-Type": "application/json"}
+    response = requests.request("POST", endpoint, headers=headers, data=data)
     print(response.text)
     create_event_and_return_message_id(
         message_to_send, TipoEvento.ENVIADO, 1)
@@ -38,24 +38,24 @@ def handler(event, context):
 
 
 handler({
-  "Records": [
-    {
-      "EventSource": "aws:sns",
-      "EventVersion": "1.0",
-      "EventSubscriptionArn": "arn:aws:sns:us-east-1:727881289392:api_gateway_clientes:494853e1-d7c8-4aa8-b51a-222adede5736",
-      "Sns": {
-        "Type": "Notification",
-        "MessageId": "ef3c9372-df25-5db8-b95c-cf00e057b262",
-        "TopicArn": "arn:aws:sns:us-east-1:727881289392:api_gateway_clientes",
-        "Subject": "None",
-        "Message": "{\"vendedor_id\":1,\"numero_seguimiento\":1}",
-        "Timestamp": "2023-02-24T04:29:38.044Z",
-        "SignatureVersion": "1",
-        "Signature": "q1EzS8WMCKA89g5B5Jc6Ayifk6u9HB8LvzzYl2xnk8xha7126DjjJOKNNv6NQT1jScGqvrDBxRSzL8PGDaDGIw5BxZRTmAPCJ5mJ99rQCpkJHCH6FhrqWYqj9nnuN6kiFvcQ1e2gepqzc6Q0iP2Fbo00YxU5g2S0qkc6uiHgovIQD3n/i50qJcP1wRDbxgchOY3Y5UW0SQV1kFGlQ4zkyLAuCxc9Nz3PbzBnY5BIeIMBHVIwTc3/ugw41eeWz/pQy4WyRRaHzS2D/Dz8ObkZc9kMQEu3kPuQ6C26vkpVrBIFZOHS9QQp86hOJ7SsfLFNfknY5HnBO+Sh8yDke+Oh1Q==",
-        "SigningCertUrl": "https://sns.us-east-1.amazonaws.com/SimpleNotificationService-56e67fcb41f6fec09b0196692625d385.pem",
-        "UnsubscribeUrl": "https://sns.us-east-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-east-1:727881289392:api_gateway_clientes:494853e1-d7c8-4aa8-b51a-222adede5736",
-        "MessageAttributes": {}
-      }
-    }
-  ]
-},"")
+    "Records": [
+        {
+            "EventSource": "aws:sns",
+            "EventVersion": "1.0",
+            "EventSubscriptionArn": "arn:aws:sns:us-east-1:727881289392:api_gateway_clientes:494853e1-d7c8-4aa8-b51a-222adede5736",
+            "Sns": {
+                "Type": "Notification",
+                "MessageId": "ef3c9372-df25-5db8-b95c-cf00e057b262",
+                "TopicArn": "arn:aws:sns:us-east-1:727881289392:api_gateway_clientes",
+                "Subject": "None",
+                "Message": "{\"vendedor_id\":1,\"numero_seguimiento\":1}",
+                "Timestamp": "2023-02-24T04:29:38.044Z",
+                "SignatureVersion": "1",
+                "Signature": "q1EzS8WMCKA89g5B5Jc6Ayifk6u9HB8LvzzYl2xnk8xha7126DjjJOKNNv6NQT1jScGqvrDBxRSzL8PGDaDGIw5BxZRTmAPCJ5mJ99rQCpkJHCH6FhrqWYqj9nnuN6kiFvcQ1e2gepqzc6Q0iP2Fbo00YxU5g2S0qkc6uiHgovIQD3n/i50qJcP1wRDbxgchOY3Y5UW0SQV1kFGlQ4zkyLAuCxc9Nz3PbzBnY5BIeIMBHVIwTc3/ugw41eeWz/pQy4WyRRaHzS2D/Dz8ObkZc9kMQEu3kPuQ6C26vkpVrBIFZOHS9QQp86hOJ7SsfLFNfknY5HnBO+Sh8yDke+Oh1Q==",
+                "SigningCertUrl": "https://sns.us-east-1.amazonaws.com/SimpleNotificationService-56e67fcb41f6fec09b0196692625d385.pem",
+                "UnsubscribeUrl": "https://sns.us-east-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-east-1:727881289392:api_gateway_clientes:494853e1-d7c8-4aa8-b51a-222adede5736",
+                "MessageAttributes": {}
+            }
+        }
+    ]
+}, "")
